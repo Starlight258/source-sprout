75a42db1dffdf9ee525e37d78d35ebc7eeeca26b
Mint <audwl03071@gmail.com>
2024-07-28 17:15:46 +0900

    chore: 프로젝트 환경설정

---
+++ src/main/java/com/sourcesprout/SourcesproutApplication.java
@@ -0,0 +1,13 @@
+package com.sourcesprout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourcesproutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SourcesproutApplication.class, args);
+	}
+
+}
